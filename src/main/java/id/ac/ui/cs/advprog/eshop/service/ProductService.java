39898299f10d89ca0269bfa3cf581b2138de2fc8
Alvin Zhafif Afilla <villalvin02@gmail.com>
2024-02-14 13:16:39 +0700

    restructure my project

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -3,10 +3,10 @@ package id.ac.ui.cs.advprog.eshop.service;
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import java.util.List;
 public interface ProductService {
-    public Product create(Product product);
-    public List<Product> findAll();
-    public Product getId(String id);
-    public Product editProduct(Product product);
-    public boolean deleteProduct(String id);
+     Product create(Product product);
+     List<Product> findAll();
+     Product getId(String id);
+     Product editProduct(Product product);
+     boolean deleteProduct(String id);
 
 }
